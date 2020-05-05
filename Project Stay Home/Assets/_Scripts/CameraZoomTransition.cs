9bb7823846ce7e019e840cfb489c4bc4bb7c89a0
Unknown <robert.andersen@uoit.net>
2020-05-05 14:06:05 -0400

    Hopefully this pushes properly. Click and hover mechanic works.

---
+++ Project Stay Home/Assets/_Scripts/CameraZoomTransition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraZoomTransition : MonoBehaviour
+{
+    private bool isZoomed = false;
+    public string scene;
+    public GameObject clickableObject;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (clickableObject.GetComponent<ObjectClick>().isPressed)
+        {
+            //zoom camera in
+            //position lerp vector3(this.position, clickableObject.position.25f) 
+            this.transform.position = Vector3.Lerp(this.transform.position, clickableObject.transform.position, .25f);
+            isZoomed = true;
+        }
+
+        if (isZoomed)
+        {
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        }
+    }
+    
+    
+}
---
+++ Project Stay Home/Assets/_Scripts/ObjectClick.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClick : MonoBehaviour
+{
+    public Vector3 minScale;
+    Vector3 currentScale;
+     //private Vector3 maxScale;
+    //private Vector3 targetScale;
+     //public float targetScaleAfter;
+     public Vector3 maxScale;
+     public bool isPressed = false;
+
+     // Start is called before the first frame update
+    void Start()
+    {
+        //targetScale = transform.localScale;
+        //minScale = transform.localScale;
+        //maxScale = targetScale * targetScaleAfter;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnMouseEnter()
+    {
+        Debug.Log("Mouse is over GameObject");
+        //minScale = transform.localScale;
+        transform.localScale = Vector3.Lerp(minScale, maxScale, .25f);
+    }
+
+    void OnMouseExit()
+    {
+        Debug.Log("Mouse is no longer over GameObject");
+        currentScale = transform.localScale;
+        transform.localScale = Vector3.Lerp(currentScale, minScale, .25f);
+    }
+
+    //When mouse pressed over object with collider
+    void OnMouseDown()
+    {
+        isPressed = true;
+    }
+
+}
