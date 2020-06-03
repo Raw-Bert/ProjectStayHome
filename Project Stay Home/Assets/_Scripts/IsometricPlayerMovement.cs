ae6d2e029fc2eead8463fb4a97ea7291b169122f
Unknown <robert.andersen@uoit.net>
2020-06-03 16:02:08 -0400

    Chnaged lighting in scene 2. Player can now move. Isometric view + walls and floor.

---
+++ Project Stay Home/Assets/_Scripts/IsometricPlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricPlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float movementSpeed = 1.0f;
+
+    Rigidbody rbody;
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 currentPos = rbody.position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 inputVector = new Vector3(-verticalInput, 0, horizontalInput);
+        //inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        Vector3 movement = inputVector * movementSpeed;
+        Vector3 newPos = currentPos + movement * Time.fixedDeltaTime;
+        rbody.MovePosition(newPos);
+        
+    }
+}
