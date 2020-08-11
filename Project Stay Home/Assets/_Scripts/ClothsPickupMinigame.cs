7a8751d9ab4e6cbfae319484a352f2560eebdf41
EmFresh <emory.wynn@uoit.net>
2020-08-11 03:34:32 -0400

    Cloths icons disappear when you put cloths in basket

--- Project Stay Home/Assets/_Scripts/ClothsPickupMinigame.cs
+++ Project Stay Home/Assets/_Scripts/ClothsPickupMinigame.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClothsPickupMinigame : MonoBehaviour
 {
+    public GameObject canvas;
+
     List<GameObject> clothing = new List<GameObject>();
+    List<GameObject> icons = new List<GameObject>();
 
     /// <summary>
     /// OnTriggerStay is called once per frame for every Collider other
@@ -16,6 +20,7 @@ public class ClothsPickupMinigame : MonoBehaviour
     {
         GameObject cloths, basket;
 
+        //left click || keycode
         if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.E))
         {
             //Pick up clothing
@@ -23,6 +28,7 @@ public class ClothsPickupMinigame : MonoBehaviour
             {
                 if (!cloths.GetComponent<Rigidbody>())
                 {
+                    clothingPickup(cloths);
                     clothing.Add(cloths);
                     cloths.SetActive(false);
                 }
@@ -35,6 +41,25 @@ public class ClothsPickupMinigame : MonoBehaviour
             }
         }
     }
+    void clothingPickup(GameObject cloths)
+    {
+        var ui = cloths.GetComponentInChildren<RawImage>();
+        var uiTrans = ui.GetComponent<RectTransform>();
+
+        ui.transform.SetParent(canvas.transform);
+        uiTrans.position = Vector3.zero;
+        uiTrans.rotation = Quaternion.identity;
+        uiTrans.localScale = Vector3.one;
+        uiTrans.ForceUpdateRectTransforms();
+
+        uiTrans.anchoredPosition = new float2(0, 0);
+        if (canvas.transform.childCount > 2)
+        {
+            var lastItem = canvas.transform.GetChild(canvas.transform.childCount - 2).GetComponent<RectTransform>();
+            uiTrans.anchoredPosition = (float2)lastItem.anchoredPosition + new float2(lastItem.sizeDelta.x, 0);
+        }
+        icons.Add(ui.gameObject);
+    }
 
     void clothingDrop(GameObject basket)
     {
@@ -45,8 +70,14 @@ public class ClothsPickupMinigame : MonoBehaviour
                 (GetComponent<Collider>().bounds.max * new float3(0, 1, 0)) +
                 (float3)cloth.GetComponent<Collider>().bounds.max * new float3(0, 1, 0) *
                 count++;
+
+            var collider = cloth.AddComponent<MeshCollider>();
+            collider.convex = true;
             cloth.AddComponent<Rigidbody>();
             cloth.SetActive(true);
         }
+        foreach(var icon in icons)
+            Destroy(icon);
+        icons.Clear();
     }
 }
