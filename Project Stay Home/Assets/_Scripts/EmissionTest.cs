a03fe0d5bd9c61ea358144929c7760f3909535fa
Unknown <robert.andersen@uoit.net>
2020-08-12 17:05:50 -0400

    cleaning up + rimlight testing stuff.

---
+++ Project Stay Home/Assets/_Scripts/EmissionTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmissionTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //mat.SetFloat("_EmissiveExposureWeight", myValue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
