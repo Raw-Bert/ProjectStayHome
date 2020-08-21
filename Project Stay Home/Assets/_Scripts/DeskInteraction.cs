bd7909006864348b7d1edb3e133088b6f4a68082
Unknown <robert.andersen@uoit.net>
2020-08-21 17:08:29 -0400

    Rimlight is 10/10

--- Project Stay Home/Assets/_Scripts/DeskInteraction.cs
+++ Project Stay Home/Assets/_Scripts/DeskInteraction.cs
@@ -7,7 +7,16 @@ public class DeskInteraction : MonoBehaviour
 {
     public bool interactableDesk = false;
     public GameObject hitBox;
+    public Material RimLight;
 
+    Material Desk;
+    
+    void Start ()
+    {
+        //RimLight = Resources.Load<Material>("RimLight");
+        //MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+    }
+    
     private void OnMouseDown()
     {
         // Trigger camera zoom in to the desk when the camera is not in the 
@@ -22,6 +31,14 @@ public class DeskInteraction : MonoBehaviour
             {
                 interactableDesk = true;
                 Debug.Log(hitBox.GetComponent<GlowWhenNear>().glow);
+                RimLight.SetFloat("Vector1_82F64F9B", 1.3f);
+                //Material Desk = meshRenderer.material;
+                //meshRenderer.material = RimLight;
+
+            }
+            else
+            {
+                RimLight.SetFloat("Vector1_82F64F9B", 40);
             }
         }
     }
